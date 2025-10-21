cb8c9b17a3ba895d32f05fee2dd7db6e8e1deffb
Siddhant Giri <210677110+SiddhantGiri-dev@users.noreply.github.com>
2025-10-21 20:41:39 +0530

    Add tasks to the UI

--- scripts/Todo.js
+++ scripts/Todo.js
@@ -3,5 +3,20 @@ export class Todo {
         this.title = title;
         this.details = details;
         this.subTodos = [];
+
+        this.element = document.createElement("div");
+        this.element.classList.add("task");
+        this.element.title = "Drag to change order";
+        this.element.innerHTML = `<input type="checkbox" title="Mark as done" />
+                                  <h3>${title}</h3>
+                                  <p>
+                                      ${details}
+                                  </p>
+                                  <div class="btns-container">
+                                      <button class="button-round btn-details">&#128462; Details</button>
+                                      <button class="button-round btn-delete">&#128462; Delete</button>
+                                  </div>`;
+
+        insertContainer.insertAdjacentElement("afterbegin", this.element);
     }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import { mainInpAnim } from "./dynamicStyles.js";
+import { Todo } from "./todo.js";
 
 const mainInp = document.getElementById("main-input");
 const addBtn = document.getElementById("add-task-btn");
@@ -8,3 +9,24 @@ const tasksContainer = document.getElementById("tasks-container");
 const detailsInp = document.getElementById("details-input");
 
 mainInpAnim(mainInp, detailsInp, addBtn);
+
+const insertTodo = () => {
+    const title = mainInp.value.trim();
+    const details = detailsInp.value.trim();
+
+    if (title) {
+        const todo = new Todo(title, details, tasksContainer);
+
+        // clearing the input fields:
+        mainInp.value = "";
+        detailsInp.value = "";
+    }
+}
+
+// Adding Todos
+addBtn.addEventListener("click", insertTodo);
+mainInp.addEventListener("keydown", (e) => {
+    if(e.code == "Enter") {
+        insertTodo();
+    }
+})
